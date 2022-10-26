cf3f3d591401ecf1e943ef3f52d1601f821d21c6
kadenhargrove <kadenhargrove@gmail.com>
2022-10-25 21:42:23 -0500

    fixed profile class name

--- profile.py
+++ profile.py
@@ -1,6 +1,6 @@
 # This files contains the profile class that contains user information
 
-class User:
+class Profile:
     def __init__(self, first_name, last_name, bio):
         self.first_name = first_name
         self.last_name = last_name
