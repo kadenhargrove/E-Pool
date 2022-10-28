feae84c75456a1d40fa01897f51cf6be6b4fafa2
albert nytko <albertnytko0@gmail.com>
2022-10-27 20:04:41 -0500

    Website template

---
+++ ticket.py
@@ -0,0 +1,36 @@
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
+
+import user from User
+
+class Ticket():
+
+    def __init__(self, info, ticketID)
+        self.info = info
+        self.ticketID = ticketID
+        self.comment = comment
+
+
+    def createTicket(self, info, ticketID):
+        
+       
+
+    def deleteTicket(self, ticketID):
+
+
+
+    def modifyTicket(self, ticketID):
+
+
+
+    def commentTicket(self, ticketID):
+
+
+
+    def likePost():
+
+
+
+        
+
+    
