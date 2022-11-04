2e67a68a2481ebb646140fd05f742507c1ba25b8
albert nytko <albertnytko0@gmail.com>
2022-11-03 22:53:58 -0500

    Cleaned up site a little

--- ticket.py
+++ ticket.py
@@ -1,19 +1,28 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = "kjhdolfuhqwp947rq9hfpiau23r4098"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///epool.db'
 
 import user from User
 
 class Ticket():
 
-    def __init__(self, info, ticketID)
+    def __init__(self, info, ticketID, comment):
         self.info = info
         self.ticketID = ticketID
         self.comment = comment
 
-
+    @app.route("/CreateTicket", methods=["POST", "GET"])
     def createTicket(self, info, ticketID):
-        
-       
+        if request.method == "POST":
+            data_form = request.form["Name", "Location", "Time", "vehicleType"]
+
+
+
+ 
 
     def deleteTicket(self, ticketID):
 
