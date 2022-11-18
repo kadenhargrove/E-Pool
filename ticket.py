837bba36e3db251b209562848479f78ce66c7149
albert nytko <albertnytko0@gmail.com>
2022-11-18 17:16:51 -0600

    added ticketform css file

--- ticket.py
+++ ticket.py
@@ -18,19 +18,49 @@ class Ticket():
         if request.method == "POST":
             data_form = request.form["Name", "Location", "Time", "vehicleType"]
             
-
+    @ticket.route("/deleteTicket", methods=["POST", "GET"])
     def deleteTicket(self, ticketID):
-        pass
+        
+        """
+        delete the item in the database that matches the specified
+        id in the url
+        """
+        qry = db.query(ticketID).filter(
+        ticket.id==id)
+        ticket = qry.first()
 
+        if ticket:
+            data_form = request.form(formdata=request.form, obj=ticket)
+            if request.method == 'POST' and data_form.validate():
+                # delete the item from the database
+                db.delete(ticket)
+                db.commit()
 
+                flash('ticket deleted successfully')
+            
+
+    @ticket.route("/modifyTicket", methods=["POST", "GET"])
     def modifyTicket(self, ticketID):
-        pass
+        qry = db.query(ticketID).filter(
+        ticket.id==id)
+        ticket = qry.first()
 
+        if ticket:
+            data_form = request.form(formdata=request.form, obj=ticket)
+            if request.method == 'POST' and data_form.validate():
+                # modify ticket form database
+                data_form = request.form["Name", "Location", "Time", "vehicleType"]
+                
+        
 
+    @ticket.route("/commentTicket", methods=["POST", "GET"])
     def commentTicket(self, ticketID):
-        pass
+        qry = db.query(ticketID).filter(
+        ticket.id==id)
+        ticket = qry.first()
+        
 
-
+    @ticket.route("/likePost", methods=["POST", "GET"])
     def likePost():
         pass
 
