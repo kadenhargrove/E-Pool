b0189ea1ed2e632a9b0b71a6187abfc94adc1918
kadenhargrove <kadenhargrove@gmail.com>
2022-12-04 19:07:31 -0600

    finish user model tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,15 @@
 import pytest
 
 from init import create_app
-from models import db
+from models import db, Users
 
 @pytest.fixture
 def app():
     app = create_app(db)
     yield app
+
+@pytest.fixture(scope='module')
+def new_user():
+    user = Users(email='khargr2@ilstu.edu', username='khargr2', password='Iloveepool123',
+                first_name='Kaden', last_name='Hargrove', bio='Hello', frequent_locations='The Bone')
+    return user
--- tests/test_models.py
+++ tests/test_models.py
@@ -3,19 +3,44 @@ This file (test_models.py) contains the unit tests for the models.py file.
 """
 from models import Users
 
-def test_new_user():
+def test_new_user(new_user):
     """
     GIVEN a User model
     WHEN a new User is created
-    THEN check the email, hashed_password, and role fields are defined correctly
+    THEN check the email, username, password, and other fields are defined correctly
     """
-    user = Users(email='khargr2@ilstu.edu', username='khargr2', password='Iloveepool123',
-                first_name='Kaden', last_name='Hargrove', bio='Hello', frequent_locations='The Bone')
 
-    assert user.email == 'khargr2@ilstu.edu'
-    assert user.username == 'khargr2'
-    assert user.password == 'Iloveepool123'
-    assert user.first_name == 'Kaden'
-    assert user.last_name == 'Hargrove'
-    assert user.bio == 'Hello'
-    assert user.frequent_locations == 'The Bone'
+    assert new_user.email == 'khargr2@ilstu.edu'
+    assert new_user.username == 'khargr2'
+    assert new_user.password == 'Iloveepool123'
+    assert new_user.first_name == 'Kaden'
+    assert new_user.last_name == 'Hargrove'
+    assert new_user.bio == 'Hello'
+    assert new_user.frequent_locations == 'The Bone'
+
+def test_set_password(new_user):
+    """
+    GIVEN an existing User
+    WHEN a new password for the user is set
+    THEN check the password is stored correctly and not as plaintext
+    """
+    new_user.set_password('NewPassword123')
+
+    assert new_user.password != 'NewPassword123'
+    assert new_user.check_password('NewPassword123')
+    assert not new_user.check_password('NewPassword1234')
+    assert not new_user.check_password('Iloveepool123')
+
+def test_check_password(new_user):
+    """
+    GIVEN an existing User
+    WHEN another new password for the user is set
+    THEN check the password is stored correctly and not as plaintext
+    """
+    new_user.set_password('Foobar123')
+
+    assert new_user.password != 'Foobar123'
+    assert new_user.check_password('Foobar123')
+    assert not new_user.check_password('Foobar1234')
+    assert not new_user.check_password('NewPassword123')
+    assert not new_user.check_password('Iloveepool123')
