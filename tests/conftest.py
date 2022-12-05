b075aaee182d2275e9ccdf1aee6b512e3ad4de53
kadenhargrove <kadenhargrove@gmail.com>
2022-12-04 20:01:00 -0600

    finish model.py tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
 from init import create_app
-from models import db, Users
+from models import db, Users, Friends, Blocks, Driver, Rider, Tickets
 
 @pytest.fixture
 def app():
@@ -13,3 +13,28 @@ def new_user():
     user = Users(email='khargr2@ilstu.edu', username='khargr2', password='Iloveepool123',
                 first_name='Kaden', last_name='Hargrove', bio='Hello', frequent_locations='The Bone')
     return user
+
+@pytest.fixture(scope='module')
+def new_friends():
+    friends = Friends(friender_username='kaden123', friend_username='emannuel123')
+    return friends
+
+@pytest.fixture(scope='module')
+def new_blocks():
+    blocks = Blocks(blocker_username='emannuel123', blocked_username='albert123')
+    return blocks
+
+@pytest.fixture(scope='module')
+def new_driver():
+    driver = Driver(email='wilson2@ilstu.edu', username='wilson2', password='mrwilson123', driver_rating=5)
+    return driver
+
+@pytest.fixture(scope='module')
+def new_rider():
+    rider = Rider(email='smith2@ilstu.edu', username='smith2', password='mrsmith123', rider_rating=4)
+    return rider
+
+@pytest.fixture(scope='module')
+def new_ticket():
+    ticket = Tickets(title='Pick me up', date_posted=(2022, 12, 4), content="Im at the Bone.", author='khargr2')
+    return ticket
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,8 +1,9 @@
 """
 This file (test_models.py) contains the unit tests for the models.py file.
+All new_* have been created in conftest.py
 """
-from models import Users
 
+# test User model
 def test_new_user(new_user):
     """
     GIVEN a User model
@@ -10,12 +11,25 @@ def test_new_user(new_user):
     THEN check the email, username, password, and other fields are defined correctly
     """
 
+    assert not new_user.email == 'khargr2@gmail.edu'
     assert new_user.email == 'khargr2@ilstu.edu'
+
+    assert not new_user.username == 'khargr3'
     assert new_user.username == 'khargr2'
+
+    assert not new_user.password == 'Iloveepool1234'
     assert new_user.password == 'Iloveepool123'
+
+    assert not new_user.first_name == 'Kaden1'
     assert new_user.first_name == 'Kaden'
+
+    assert not new_user.last_name == 'Hargrove1'
     assert new_user.last_name == 'Hargrove'
+
+    assert not new_user.bio == 'Hello1'
     assert new_user.bio == 'Hello'
+
+    assert not new_user.frequent_locations == ''
     assert new_user.frequent_locations == 'The Bone'
 
 def test_set_password(new_user):
@@ -44,3 +58,91 @@ def test_check_password(new_user):
     assert not new_user.check_password('Foobar1234')
     assert not new_user.check_password('NewPassword123')
     assert not new_user.check_password('Iloveepool123')
+
+# test Friends model
+def test_new_friends(new_friends):
+    """
+    GIVEN a Friends model
+    WHEN a new Frienship is created
+    THEN check the friender_username and friend_username fields are defined correctly
+    """
+
+    assert not new_friends.friender_username == 'kaden1234'
+    assert new_friends.friender_username == 'kaden123'
+
+    assert not new_friends.friend_username == 'emannuel1234'
+    assert new_friends.friend_username == 'emannuel123'
+
+# test Blocks model
+def test_new_blocks(new_blocks):
+    """
+    GIVEN a Blocks model
+    WHEN a new Blockship is created
+    THEN check the blocker_username and blocked_username fields are defined correctly
+    """
+
+    assert not new_blocks.blocker_username == 'emannuel1234'
+    assert new_blocks.blocker_username == 'emannuel123'
+    
+    assert not new_blocks.blocked_username == 'albert1234'
+    assert new_blocks.blocked_username == 'albert123'
+
+# test Driver model
+def test_new_driver(new_driver):
+    """
+    GIVEN a Driver model
+    WHEN a new Driver is created
+    THEN check the email, username, password, and driver_rating fields are defined correctly
+    """
+
+    assert not new_driver.email == 'wilso@ilstu.edu'
+    assert new_driver.email == 'wilson2@ilstu.edu'
+
+    assert not new_driver.username == 'wilson1'
+    assert new_driver.username == 'wilson2'
+
+    assert not new_driver.password == 'yo123'
+    assert new_driver.password == 'mrwilson123'
+
+    assert not new_driver.driver_rating == 4
+    assert new_driver.driver_rating == 5
+
+# test Rider model
+def test_new_rider(new_rider):
+    """
+    GIVEN a Rider model
+    WHEN a new Rider is created
+    THEN check the email, username, password, and rider_rating fields are defined correctly
+    """
+
+    assert not new_rider.email == 'smit@ilstu.edu'
+    assert new_rider.email == 'smith2@ilstu.edu'
+
+    assert not new_rider.username == 'smith1'
+    assert new_rider.username == 'smith2'
+
+    assert not new_rider.password == 'hey123'
+    assert new_rider.password == 'mrsmith123'
+
+    assert not new_rider.rider_rating == 3
+    assert new_rider.rider_rating == 4
+
+# test Tickets model
+def test_new_ticket(new_ticket):
+    """
+    GIVEN a Ticket model
+    WHEN a new Ticket is created
+    THEN check the title, date_posted, content, and author fields are defined correctly
+    """
+
+    assert not new_ticket.title == 'Pick me up!!!'
+    assert new_ticket.title == 'Pick me up'
+    
+    assert not new_ticket.date_posted == (2022, 12, 5)
+    assert new_ticket.date_posted == (2022, 12, 4)
+
+    assert not new_ticket.content == 'Im not at the Bone.'
+    assert new_ticket.content == 'Im at the Bone.'
+
+    assert not new_ticket.author == 'khargr1'
+    assert new_ticket.author == 'khargr2'
