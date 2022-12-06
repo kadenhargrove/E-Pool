7c3ff1e1f5d2b8432e526513e0c46c08e017b442
Eman <emanasekun@gmail.com>
2022-12-06 05:25:58 -0600

    finished up profile testing

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -27,11 +27,11 @@ def test_edit_profile(init_database, test_client):
     assert response.status_code == 200
 
     response = test_client.post('/editprofile', data=dict(
-    first_name='User',
-    last_name='Name',
+    fname='User',
+    lname='Name',
     bio='This is my profile',
-    frequent_locations='Chicago, IL'
-), follow_redirects=True)
+    freqLoc='Chicago, IL'
+    ), follow_redirects=True)
 
     # Ensure that the request was successful (response code 200)
     assert response.status_code == 200
@@ -41,29 +41,77 @@ def test_edit_profile(init_database, test_client):
     assert b"This is my profile" in response.data
     assert b"Chicago, IL" in response.data
     
-#def test_delete_account(init_database, test_client):
+def test_delete_account(init_database, test_client):
+    # First, we need to log in to the application
+    test_client.post('/login',
+                     data=dict(email='defaultuser@gmail.com', psw='Epoolbetterthanuber'), 
+                     follow_redirects=True)
+
+    # Next, we can test the /deleteaccount route
+    response = test_client.post('/deleteaccount', data=dict(psw1='Epoolbetterthanuber', psw2='Epoolbetterthanuber'), follow_redirects=True)
+
+    assert response.status_code == 200
+
+    #next we need to test that the two passwords are the same
+    assert b"Your account has been successfully deleted.", "sucess"
     
-    
-    
+    #next we need to test what happens when password 2 is incorrect
+    response = test_client.post('/deleteaccount', data=dict(psw1='Epoolbetterthanuber', psw2='Epoolbetterthar'), follow_redirects=True)
+
+    assert b"Passwords do not match.", "warning"
+
+    #next we need to test what happens when password 1 is incorrect
+    response = test_client.post('/deleteaccount', data=dict(psw1='Epoolbetterthan', psw2='Epoolbetterthanuber'), follow_redirects=True)
+    assert b"Password incorrect!", "error"
+
+
 
 def test_add_friend(init_database, test_client):
+    # First, we need to log in to the application
+    test_client.post('/login',
+                     data=dict(email='defaultuser@gmail.com', psw='Epoolbetterthanuber'), 
+                     follow_redirects=True)
+                     
      # add the user as a friend
     response = test_client.post('/profile/shaqoneil/addfriend', data=dict(
         friender_username="default",
         friend_username="shaqoneil"
     ), follow_redirects=True)
     assert response.status_code == 200
-     # get the friends page
-    test_client.post('/login',
-                     data=dict(email='defaultuser@gmail.com', psw='Epoolbetterthanuber'), 
-                     follow_redirects=True)
+    assert b"Friend added!", "success"
 
-    response = test_client.get('/friends', follow_redirects=True)
+    # check if user is already your friend
+    response = test_client.post('/profile/shaqoneil/addfriend', data=dict(
+        friender_username="default",
+        friend_username="shaqoneil"
+    ), follow_redirects=True)
     assert response.status_code == 200
-    # verify that the friend was added
-    assert b"shaqoneil" in response.data
+    assert b"Already friends!", "success"
 
+    # cannot friend yourself
+    response = test_client.post('/profile/default/addfriend', data=dict(
+        friender_username="default",
+        friend_username="shaqoneil"
+    ), follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Cannot friend self!", "warning"
 
+    # cannot friend a blocked user
+    response = test_client.post('/profile/shaqoneil/block', data=dict(
+        blocker_username="default",
+        blocked_username="shaqoneil"
+    ), follow_redirects=True)
+
+    assert response.status_code == 200
+
+    response = test_client.post('/profile/shaqoneil/addfriend', data=dict(
+        friender_username="default",
+        friend_username="shaqoneil"
+    ), follow_redirects=True)
+
+    assert b"Cannot friend blocked user!", "warning"
+
+    
 def test_delete_friend(init_database, test_client):
     # add the user as a friend
     response = test_client.post('/profile/shaqoneil/addfriend', data=dict(
@@ -89,6 +137,21 @@ def test_block(init_database, test_client):
     assert response.status_code == 200
     assert b"User blocked!", "success" in response.data
     
+    # user is already blocked
+    response = test_client.post('/profile/shaqoneil/block', data=dict(
+        blocker_username="default",
+        blocked_username="shaqoneil"
+    ), follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Already blocked user!", "success" in response.data
+
+    # cannot block yourself
+    response = test_client.post('/profile/default/block', data=dict(
+        blocker_username="default",
+        blocked_username="shaqoneil"
+    ), follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Cannot block self!", "warning" in response.data
     
 def test_unblock(init_database, test_client):
     # block the user
